64a347024076374e920a6cb81e8fd4c95439505c
Michelle Nkosi <michellen.nkosi@gmail.com>
2024-07-30 23:24:22 +0200

    Update Printf.c

--- Printf.c
+++ Printf.c
@@ -1,9 +1,7 @@
 #include "main.h"
-/**
- * _printf - Produces output according to a format.
- * @format: A character string containing zero or more directives.
- * Return: The number of characters printed (excluding the null byte).
- */
+/* _printf - Produces output according to a format. */
+/* @format: A character string containing zero or more directives.*/
+/* Return: The number of characters printed (excluding the null byte).*/
 int _printf(const char *format, ...)
 {
 int count = 0;
