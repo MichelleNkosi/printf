7b7699c0524e8adbdbadb46bc7f77903316bc4ce
Michelle Nkosi <michellen.nkosi@gmail.com>
2024-07-30 21:46:58 +0200

    Update printf.c

--- printf.c
+++ printf.c
@@ -1,57 +1,71 @@
-#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
 
 /**
- * printf - gives output according to a format.
- * @format: A character string with format specifiers.
- * Return: The number of characters printed null.
+ * _printf - Gives output according to a format.
+ * @format: The format string containing the characters and the specifiers.
+ * Return: The number of characters printed excluding the null byte.
  */
 int _printf(const char *format, ...)
 {
-va_list args;
-int count = 0;
-const char *p;
-char *str;
-char ch;
+    va_list args;
+    int i = 0, count = 0;
+    char *str;
 
-if (format == NULL)
-return (-1);
+    if (format == NULL)
+        return (-1);
 
-va_start(args, format);
-for (p = format; *p != '\0'; p++)
-{
-if (*p == '%')
-{
-p++;
-if (*p == '\0')
-break;
-if (*p == 'c')
-{
-ch = (char)va_arg(args, int);
-count += write(1, &ch, 1);
+    va_start(args, format);
+
+    while (format[i])
+    {
+        if (format[i] == '%')
+        {
+            i++;
+            switch (format[i])
+            {
+                case 'c':
+                    count += write(1, &va_arg(args, int), 1);
+                    break;
+                case 's':
+                    str = va_arg(args, char *);
+                    if (str == NULL)
+                        str = "(null)";
+                    count += write(1, str, _strlen(str));
+                    break;
+                case '%':
+                    count += write(1, "%", 1);
+                    break;
+                default:
+                    count += write(1, &format[i - 1], 1);
+                    count += write(1, &format[i], 1);
+                    break;
+            }
+        }
+        else
+        {
+            count += write(1, &format[i], 1);
+        }
+        i++;
+    }
+
+    va_end(args);
+    return (count);
 }
-else if (*p == 's')
+
+/**
+ * _strlen - Calculates the length of a string.
+ * @s: The string to be measured.
+ * Return: The length of the string.
+ */
+int _strlen(const char *s)
 {
-str = va_arg(args, char *);
-if (str == NULL)
-str = "(null)";
-while (*str)
-count += write(1, str++, 1);
-}
-else if (*p == '%')
-{
-count += write(1, "%", 1);
-}
-else
-{
-count += write(1, "%", 1);
-count += write(1, p, 1);
-}
-}
-else
-{
-count += write(1, p, 1);
-}
-}
-va_end(args);
-return (count);
+    int len = 0;
+
+    while (s[len] != '\0')
+        len++;
+    
+    return (len);
 }
