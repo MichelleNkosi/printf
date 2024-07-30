52b1891fa2586821ea378278bcac833c092ea893
Michelle Nkosi <michellen.nkosi@gmail.com>
2024-07-30 23:18:13 +0200

    Update Printf.c

--- Printf.c
+++ Printf.c
@@ -9,50 +9,50 @@
  */
 int _printf(const char *format, ...)
 {
-    int count = 0;
-    int i = 0;
-    va_list args;
+int count = 0;
+int i = 0;
+va_list args;
 
-    va_start(args, format);
+va_start(args, format);
 
-    if (!format)
-        return (-1);
+if (!format)
+return (-1);
 
-    while (format && format[i])
-    {
-        if (format[i] == '%' && format[i + 1] != '\0')
-        {
-            i++;
-            switch (format[i])
-            {
-                case 'c':
-                    count += putchar(va_arg(args, int));
-                    break;
-                case 's':
-                {
-                    char *str = va_arg(args, char *);
-                    if (!str)
-                        str = "(null)";
-                    while (*str)
-                        count += putchar(*str++);
-                    break;
-                }
-                case '%':
-                    count += putchar('%');
-                    break;
-                default:
-                    count += putchar('%');
-                    count += putchar(format[i]);
-                    break;
-            }
-        }
-        else
-        {
-            count += putchar(format[i]);
-        }
-        i++;
-    }
+while (format && format[i])
+{
+if (format[i] == '%' && format[i + 1] != '\0')
+{
+i++;
+switch (format[i])
+{
+case 'c':
+count += putchar(va_arg(args, int));
+break;
+case 's':
+{
+char *str = va_arg(args, char *);
+if (!str)
+str = "(null)";
+while (*str)
+count += putchar(*str++);
+break;
+}
+case '%':
+count += putchar('%');
+break;
+default:
+count += putchar('%');
+count += putchar(format[i]);
+break;
+}
+}
+else
+{
+count += putchar(format[i]);
+}
+i++;
+}
 
-    va_end(args);
-    return (count);
+va_end(args);
+return (count);
 }
