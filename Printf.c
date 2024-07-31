d43c9fe65cee7a26484dd79b2a896365302fbd27
MichelleNkosi <Michellen.nkosi@gmail.com>
2024-07-31 03:59:25 -0700

    Implemented _printf function with specifiers c, s, %, d, i

--- Printf.c
+++ Printf.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
 
 /**
  * _printf - Produces output according to a format
@@ -11,11 +13,13 @@ int _printf(const char *format, ...)
 int count = 0;
 const char *p;
 va_list args;
+
 va_start(args, format);
 if (!format)
 {
 return (-1);
 }
+
 for (p = format; *p != '\0'; p++)
 {
 if (*p == '%')
@@ -39,6 +43,45 @@ while (*s)
 {
 write(1, s, 1);
 s++;
+count++;
+}
+break;
+}
+case 'd':
+case 'i':
+{
+int num = va_arg(args, int);
+char buf[50];
+int i = 0, j, is_negative = 0;
+                
+if (num == 0)
+{
+write(1, "0", 1);
+count++;
+break;
+}
+                
+if (num < 0)
+{
+is_negative = 1;
+num = -num;
+}
+
+while (num > 0)
+{
+buf[i++] = (num % 10) + '0';
+num /= 10;
+}
+                
+if (is_negative)
+{
+write(1, "-", 1);
+count++;
+}
+                
+for (j = i - 1; j >= 0; j--)
+{
+write(1, &buf[j], 1);
 count++;
 }
 break;
--- main.c
+++ main.c
@@ -25,5 +25,21 @@ printf("count: %d\n", count);
 count = _printf("Unknown specifier: %x\n");
 printf("count: %d\n", count);
 
+    /* integer conversion test with 'd' */
+count = _printf("Integer (d): %d\n", 12345);
+printf("count: %d\n", count);
+
+    /* negative integer conversion test with 'd' */
+count = _printf("Negative Integer (d): %d\n", -6789);
+printf("count: %d\n", count);
+
+    /* integer conversion test with 'i' */
+count = _printf("Integer (i): %i\n", 54321);
+printf("count: %d\n", count);
+
+    /* negative integer conversion test with 'i' */
+count = _printf("Negative Integer (i): %i\n", -9876);
+printf("count: %d\n", count);
+
 return (0);
 }
